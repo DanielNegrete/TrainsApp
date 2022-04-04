8b129221dcce4e845a6d1a03ec010b51c72aa809
DanielNegrete <68568310+DanielNegrete@users.noreply.github.com>
2022-04-04 09:52:19 -0500

    Update user controller

--- user/src/main/java/com/user/app/controller/UserController.java
+++ user/src/main/java/com/user/app/controller/UserController.java
@@ -1,4 +1,4 @@
-package com.user.app.controller;
+package com.train.app.controller;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,8 +10,6 @@ import javax.validation.constraints.NotNull;
 import org.apache.log4j.Logger;
 import org.apache.log4j.xml.DOMConfigurator;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,92 +18,83 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.user.app.dto.LoginRequestDTO;
-import com.user.app.dto.ResponseDTO;
-import com.user.app.dto.TrainRequestDTO;
-import com.user.app.dto.UserRequestDTO;
-import com.user.app.entity.User;
-import com.user.app.model.Booking;
-import com.user.app.model.Train;
-import com.user.app.service.UserService;
+import com.train.app.dto.TrainRequestDTO;
+import com.train.app.entity.Train;
+import com.train.app.service.TrainService;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/train")
 @Validated
-public class UserController {
+public class TrainController {
 	
-	private static final Logger logger = Logger.getLogger(UserController.class);
+	private static final Logger logger = Logger.getLogger(TrainController.class);
 	
 	{DOMConfigurator.configure("src/main/resources/log4j.xml");}
 	
 	@Autowired
-	UserService userService;
+	TrainService trainService;
 	
 	@GetMapping()
-	public List<User> getUsers(){
-		logger.info("Getting all the users");
-		return userService.getUsers();
+	public List<Train> getTrains(){
+		logger.info("Getting all the trains info");
+		return trainService.getTrains();
 	}
 	
-	@PostMapping("/save")
-	public User saveUser(@Valid @RequestBody UserRequestDTO userRequestDTO) {
-		logger.debug("User: " + userRequestDTO.getUsername() + " got saved");
-		return userService.saveUsers(userRequestDTO);
+	@PostMapping()
+	public Train saveTrain(@Valid @RequestBody TrainRequestDTO trainRequestDTO) {
+		logger.debug("Train: " + " got saved");
+		return this.trainService.saveTrains(trainRequestDTO);
 	}
-    
-    @GetMapping("/user/{userId}")
-    public Optional<User> validateUser(@NotNull @Min(1) @PathVariable("userId") Integer userId) {
-    	logger.info(" Validating if the user: " + userId + " exist or not");
-    	return userService.getUserByUserId(userId);
-    }
-    
-    @PostMapping("/login")
-    public ResponseEntity<ResponseDTO> login(@Valid @RequestBody LoginRequestDTO loginRequestDTO){
-    	ResponseDTO response = userService.login(loginRequestDTO);
-    	return new ResponseEntity<ResponseDTO>(response, HttpStatus.OK);
-    }
-    
-    @GetMapping("/booking/{userId}")
-    public List<Booking> getBookingsByUserId(@NotNull @Min(1) @PathVariable("userId") Integer userId){
-    	logger.info("Getting the bookings of the user: " + userId);
-    	return userService.getBookings(userId);
-    }
-    
-    @PostMapping("/train/trainssource")
-    public List<Train> getTrainsBySourceDestinationDate(@Valid @RequestBody TrainRequestDTO trainRequestDTO){
-    	return userService.getJourneys(trainRequestDTO);
-    }
-    
-    
-    
-    
-    
 
+
+    @GetMapping("/{trainId}")
+    public Optional<Train> getTrainByTrainId(@NotNull @Min(1) @PathVariable("trainId") Integer trainId){
+    	logger.info("Getting the train info of: " + trainId);
+    	return trainService.getTrainByTrainId(trainId);
+    }
     
-    /*
-    @GetMapping( path = "/{userId}")
-    public Optional<User> getUsersById(@PathVariable("userId") Integer userId) {
-    	logger.info("Getting the info of user: " + userId);
-        return this.userService.getById(userId);
+    @PostMapping("/trainssource")
+    public List<Train> getTrainsBySourceDestinationDate(@Valid @RequestBody TrainRequestDTO trainRequestDTO){
+    	return trainService.getTrainsBySourceDestinationDate(trainRequestDTO);
     }
     
     
-    @GetMapping("/query")
-    public Optional<User> getUserByUsername(@RequestParam("username") String username){
-    	logger.info("Getting the info of the user: " + username);
-        return userService.getByUsername(username);
-    } 
     
-    @DeleteMapping( path = "/{userId}")
-    public String deleteById(@PathVariable("userId") Integer userId){
-        boolean ok = this.userService.deleteUser(userId);
+    
+    
+    
+    //
+    /*
+         @GetMapping("/journeys")
+    public List<Train> getJourneys(@RequestParam("source") String source, @RequestParam("destination") String destination, @RequestParam("date") String date) {
+    	LocalDate localDate = LocalDate.parse(date);
+    	logger.info("Getting the train info using the parameters Source:" + source + " Destination: " + destination + " Date: " + date);
+    	return trainService.getTrainsPerParams(source, destination, localDate);
+    }
+    
+        
+    @DeleteMapping( path = "/{trainId}")
+    public String deleteById(@PathVariable("trainId") Integer trainId){
+        boolean ok = this.trainService.deleteTrain(trainId);
         if (ok){
-        	logger.debug("User: " + userId + " got deleted");
-            return "Delete User Successful id:" + userId;
+        	logger.debug("Train: " + trainId + " got deleted");
+            return "Delete Train Successful id:" + trainId;
         }else{
-        	logger.error("Error occurred while deleting user: " + userId);
-            return "Cannot Deleted User:" + userId;
+        	logger.error("Error occurred while deleting the train: " + trainId);
+            return "Cannot Deleted Train:" + trainId;
         }
     }  
-    */
+    
+        
+    @GetMapping("/query")
+    public List<Train> getTrainsByDestination(@RequestParam("destination") String destination){
+        return this.trainService.getByDestination(destination);
+    } 
+    
+        @GetMapping( path = "/{trainId}")
+    public Optional<Train> getTrainsById(@PathVariable("trainId") Integer trainId) {
+        return this.trainService.getById(trainId);
+    }
+     */
+    
 }
