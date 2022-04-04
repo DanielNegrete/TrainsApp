5515cd3af0c4015d4b10795900a0121da3b6561a
DanielNegrete <68568310+DanielNegrete@users.noreply.github.com>
2022-04-04 09:47:53 -0500

    Update user service

--- user/src/main/java/com/user/app/service/UserService.java
+++ user/src/main/java/com/user/app/service/UserService.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 
 import org.apache.log4j.Logger;
 import org.apache.log4j.xml.DOMConfigurator;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.circuitbreaker.CircuitBreaker;
 import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
@@ -15,11 +16,13 @@ import org.springframework.stereotype.Service;
 
 import com.user.app.client.BookingClient;
 import com.user.app.client.TrainClient;
-import com.user.app.controller.UserController;
 import com.user.app.dto.LoginRequestDTO;
 import com.user.app.dto.ResponseDTO;
 import com.user.app.dto.TrainRequestDTO;
+import com.user.app.dto.UserRequestDTO;
 import com.user.app.entity.User;
+import com.user.app.exception.LoginFailedException;
+import com.user.app.exception.UserNotFoundException;
 import com.user.app.model.Booking;
 import com.user.app.model.Train;
 import com.user.app.repository.UserRepository;
@@ -44,6 +47,8 @@ public class UserService {
 	@Autowired
 	TrainClient trainClient;
 	
+	
+	//Booking Client
 	public List<Booking> getBookings(Integer userId) {
 		CircuitBreaker circuitBreaker = circuitBreakerFactory.create("circuitbreaker");
 		return circuitBreaker.run(() -> bookingClient.getBookingsByUserId(userId), throwable -> getDefaultInfo());
@@ -54,22 +59,68 @@ public class UserService {
 		return Collections.emptyList();
 	}
 	
+
+	//Train Client
+	public List<Train> getJourneys(@Valid TrainRequestDTO trainRequestDTO) {
+		CircuitBreaker circuitBreaker = circuitBreakerFactory.create("circuitbreaker");
+		return circuitBreaker.run(() -> trainClient.getTrainsBySourceDestinationDate(trainRequestDTO), throwable -> getDefaultInfoTrain());
+	}
+	
+	public List<Train> getDefaultInfoTrain() {
+		logger.error("Train service is down");
+		return Collections.emptyList();
+	}
+	
+	
+	//User Client
 	public List<User> getUsers(){
 		return (List<User>) userRepository.findAll();
 	}
 	
-	public User saveUsers(User user) {
+	public User saveUsers(UserRequestDTO userRequestDTO) {
+		User user = new User();
+		BeanUtils.copyProperties(userRequestDTO, user);
 		return userRepository.save(user);
 	}
 	
-	public Optional<User> getById(Integer userId){
-		return userRepository.findById(userId);
+	public Optional<User> getUserByUserId(Integer userId){
+		Optional<User> optionalUser = userRepository.findByUserId(userId);
+		if(!optionalUser.isPresent())
+			throw new UserNotFoundException("User not found");
+		return optionalUser;
 	}
 	
+	public ResponseDTO login(LoginRequestDTO loginRequestDTO) {
+		Optional<User> optionalUser = userRepository.findByUsernameAndPassword(loginRequestDTO.getUsername(), loginRequestDTO.getPassword());
+		ResponseDTO responseDTO = new ResponseDTO("", 0);
+		
+		if(!optionalUser.isPresent()){
+			logger.warn("Wrong username of password");
+			throw new LoginFailedException("Bad Credentials");
+		}
+		else{
+			logger.info("User: " + loginRequestDTO.getUsername() + " loged into the system");
+			responseDTO.setMessage("User accepted");
+			responseDTO.setStatusCode(200);
+		}
+		return responseDTO;
+	}
+	
+
+	/*
+
+		
+ 	public Optional<User> getById(Integer userId){
+	return userRepository.findById(userId);
+}
+
 	
 	public Optional<User> getByUsername(String username)
 	{
-		return userRepository.findByUsername(username);
+		Optional<User> optionalUser = userRepository.findByUsername(username);
+		if(!optionalUser.isPresent())
+			throw new UserNotFoundException("User not found");
+		return optionalUser;
 	}
 	
 	public boolean deleteUser(Integer userId) {
@@ -80,35 +131,6 @@ public class UserService {
 			return false;
 		}
 	}
-	
-	public Optional<User> getUserByUserId(Integer userId){
-		return userRepository.findByUserId(userId);
-	}
-	
-	public ResponseDTO login(@Valid LoginRequestDTO loginRequestDTO) {
-		Optional<User> optionalUser = userRepository.findByUsernameAndPassword(loginRequestDTO.getUsername(), loginRequestDTO.getPassword());
-		ResponseDTO responseDTO = new ResponseDTO("", 0);
-		
-		if(!optionalUser.isPresent()){
-			logger.warn("Wrong username of password");
-			responseDTO.setMessage("Bad credentials");
-			responseDTO.setStatusCode(404);
-		}
-		else{
-			logger.info("User: " + loginRequestDTO.getUsername() + " loged into the system");
-			responseDTO.setMessage("User accepted");
-			responseDTO.setStatusCode(200);
-		}
-		return responseDTO;
-	}
-	
-	
-	public List<Booking> getBookingsByUserId(Integer userId){
-		return bookingClient.getBookingsByUserId(userId);
-	}
-	
-	
-	public List<Train> getTrainsBySourceDestinationDate(TrainRequestDTO trainRequestDTO){
-		return trainClient.getTrainsBySourceDestinationDate(trainRequestDTO);
-	}
+	 
+	 */
 }
