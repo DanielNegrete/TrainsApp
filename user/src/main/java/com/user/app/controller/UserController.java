5e84190b5430dd910f91f1c7659177dd4d6c6b1f
DanielNegrete <68568310+DanielNegrete@users.noreply.github.com>
2022-04-04 09:49:09 -0500

    Update user controller

--- user/src/main/java/com/user/app/controller/UserController.java
+++ user/src/main/java/com/user/app/controller/UserController.java
@@ -3,23 +3,27 @@ package com.user.app.controller;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
 import org.apache.log4j.Logger;
 import org.apache.log4j.xml.DOMConfigurator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.user.app.dto.LoginRequestDTO;
 import com.user.app.dto.ResponseDTO;
 import com.user.app.dto.TrainRequestDTO;
+import com.user.app.dto.UserRequestDTO;
 import com.user.app.entity.User;
 import com.user.app.model.Booking;
 import com.user.app.model.Train;
@@ -27,6 +31,7 @@ import com.user.app.service.UserService;
 
 @RestController
 @RequestMapping("/user")
+@Validated
 public class UserController {
 	
 	private static final Logger logger = Logger.getLogger(UserController.class);
@@ -43,16 +48,47 @@ public class UserController {
 	}
 	
 	@PostMapping("/save")
-	public User saveUser(@RequestBody User user) {
-		logger.debug("User: " + user.getUsername() + " got saved");
-		return this.userService.saveUsers(user);
+	public User saveUser(@Valid @RequestBody UserRequestDTO userRequestDTO) {
+		logger.debug("User: " + userRequestDTO.getUsername() + " got saved");
+		return userService.saveUsers(userRequestDTO);
 	}
+    
+    @GetMapping("/user/{userId}")
+    public Optional<User> validateUser(@NotNull @Min(1) @PathVariable("userId") Integer userId) {
+    	logger.info(" Validating if the user: " + userId + " exist or not");
+    	return userService.getUserByUserId(userId);
+    }
+    
+    @PostMapping("/login")
+    public ResponseEntity<ResponseDTO> login(@Valid @RequestBody LoginRequestDTO loginRequestDTO){
+    	ResponseDTO response = userService.login(loginRequestDTO);
+    	return new ResponseEntity<ResponseDTO>(response, HttpStatus.OK);
+    }
+    
+    @GetMapping("/booking/{userId}")
+    public List<Booking> getBookingsByUserId(@NotNull @Min(1) @PathVariable("userId") Integer userId){
+    	logger.info("Getting the bookings of the user: " + userId);
+    	return userService.getBookings(userId);
+    }
+    
+    @PostMapping("/train/trainssource")
+    public List<Train> getTrainsBySourceDestinationDate(@Valid @RequestBody TrainRequestDTO trainRequestDTO){
+    	return userService.getJourneys(trainRequestDTO);
+    }
+    
+    
+    
+    
+    
 
+    
+    /*
     @GetMapping( path = "/{userId}")
     public Optional<User> getUsersById(@PathVariable("userId") Integer userId) {
     	logger.info("Getting the info of user: " + userId);
         return this.userService.getById(userId);
     }
+    
     
     @GetMapping("/query")
     public Optional<User> getUserByUsername(@RequestParam("username") String username){
@@ -71,29 +107,5 @@ public class UserController {
             return "Cannot Deleted User:" + userId;
         }
     }  
-    
-    @GetMapping("/user/{userId}")
-    public Optional<User> validateUser(@PathVariable("userId") Integer userId) {
-    	logger.info(" Validating if the user: " + userId + " exist or not");
-    	return userService.getUserByUserId(userId);
-    }
-    
-    @GetMapping("/login")
-    public ResponseEntity<ResponseDTO> login(LoginRequestDTO loginRequestDTO){
-    	ResponseDTO response = userService.login(loginRequestDTO);
-    	return new ResponseEntity<ResponseDTO>(response, HttpStatus.ACCEPTED);
-    }
-    
-    @GetMapping("/booking/{userId}")
-    public List<Booking> getBookingsByUserId(@PathVariable("userId") Integer userId){
-    	logger.info("Getting the bookings of the user: " + userId);
-    	return userService.getBookings(userId);
-    }
-    
-    
-    @PostMapping("/train/trainssource")
-    public List<Train> getTrainsBySourceDestinationDate(@RequestBody TrainRequestDTO trainRequestDTO){
-    	return userService.getTrainsBySourceDestinationDate(trainRequestDTO);
-    }
-    
+    */
 }
